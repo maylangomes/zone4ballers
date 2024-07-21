e18511091397696f94b204b2a9282ba83a04cad8
Maylan <maylan.gomes@epitech.eu>
2024-07-21 17:29:03 +0200

    feat: login et sign up créés avec les redirections

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,12 +1,22 @@
 'use client'
 
-import SignupForm from './SignupForm';
+
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+    const router = useRouter();
+
     return (
         <div>
-            <h1>Welcome</h1>
-            <SignupForm />
+            <h1>Accueil</h1>
+            <br />
+            <button type="button" onClick={() => router.push('/login')}>
+                login
+            </button>
+            <br />
+            <button type="button" onClick={() => router.push('/signup')}>
+                signup
+            </button>
         </div>
     );
 }

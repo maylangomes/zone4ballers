a904dfff00d9d41f519493f67ab153d9aa72e3e3
Maylan <maylan.gomes@epitech.eu>
2024-07-30 23:54:16 +0200

    fix: delte jsconfig et layout en ts

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,14 +1,18 @@
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "?",
-  description: "??",
+export const metadata: Metadata = {
+  title: "zone4ballers"
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>

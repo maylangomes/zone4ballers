4689502abfbbb5f919fe9fc884337f386dbfdf12
Maylan <maylan.gomes@epitech.eu>
2024-08-27 18:34:17 +0200

    feat: page transporteur avec shippo

---
+++ frontend/src/app/api/controllers/shipping/route.ts
@@ -0,0 +1,59 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { Shippo, AddressCreateRequest, ParcelCreateRequest, DistanceUnitEnum, WeightUnitEnum } from 'shippo';
+
+const shippo = new Shippo({
+  apiKeyHeader: 'shippo_test_a221da6720c36b41b1e265e2fbf66fa7b9cc7453',
+});
+
+export async function POST(request: NextRequest) {
+  try {
+    const formData = await request.formData();
+    const addressTo = JSON.parse(formData.get('addressTo') as string) as AddressCreateRequest;
+
+    const addressFrom: AddressCreateRequest = {
+      name: "Maylan Gomes",
+      street1: "20 Rue de Flore",
+      city: "Eragny",
+      state: "",
+      zip: "95610",
+      country: "FR",
+      phone: "+33 6 37 26 91 05",
+      email: "maylangomes96@gmail.com",
+    };
+
+    const parcel: ParcelCreateRequest = {
+      length: "10",
+      width: "20",
+      height: "5",
+      distanceUnit: DistanceUnitEnum.In,
+      weight: "2",
+      massUnit: WeightUnitEnum.Lb,
+    };
+
+    const shipment = await shippo.shipments.create({
+      addressFrom: addressFrom,
+      addressTo: addressTo,
+      parcels: [parcel],
+      async: false,
+    });
+
+    if (shipment && shipment.rates) {
+      const detailedRates = shipment.rates.map(rate => ({
+        provider: rate.provider,
+        amount: rate.amount,
+        currency: rate.currency,
+      }));      
+
+      return NextResponse.json(detailedRates, { status: 200 });
+    } else {
+      return NextResponse.json({ error: 'Aucun tarif trouvé.' }, { status: 404 });
+    }
+  } catch (error: any) {
+    console.error('Erreur lors de la récupération des tarifs:', error);
+
+    return NextResponse.json(
+      { error: error.message || 'Erreur lors de la récupération des tarifs.' },
+      { status: 500 }
+    );
+  }
+}
---
+++ frontend/src/app/pages/shipping/page.tsx
@@ -0,0 +1,135 @@
+'use client';
+import { FormEvent, useState } from 'react';
+
+const HomePage = () => {
+  const [addressTo, setAddressTo] = useState({
+    name: '',
+    street1: '',
+    city: '',
+    state: '',
+    zip: '',
+    country: '',
+    phone: '',
+    email: '',
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [shippingRates, setShippingRates] = useState<any[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setAddressTo((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const fetchShippingRates = async (e: FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    const formData = new FormData();
+    formData.append('addressTo', JSON.stringify(addressTo));
+
+    try {
+      const response = await fetch('/api/controllers/shipping', {
+        method: 'POST',
+        body: formData,
+      });
+
+      const data = await response.json();      
+
+      if (response.ok) {
+        setShippingRates(data);
+      } else {
+        setError(data.error || 'Erreur lors de la récupération des tarifs.');
+      }
+    } catch (error: any) {
+      setError(error.message || 'Erreur lors de la récupération des tarifs.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={fetchShippingRates}>
+        <h2>Adresse de destination</h2>
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={addressTo.name}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          placeholder="Street"
+          name="street1"
+          value={addressTo.street1}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          placeholder="City"
+          name="city"
+          value={addressTo.city}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          placeholder="State"
+          name="state"
+          value={addressTo.state}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          placeholder="Zip"
+          name="zip"
+          value={addressTo.zip}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          placeholder="Country"
+          name="country"
+          value={addressTo.country}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          placeholder="Phone"
+          name="phone"
+          value={addressTo.phone}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          value={addressTo.email}
+          onChange={handleInputChange}
+        />
+
+        <button type="submit" disabled={loading}>
+          {loading ? 'Chargement...' : "Obtenir les tarifs d'expédition"}
+        </button>
+      </form>
+
+      {shippingRates.length > 0 && (
+        <ul>
+          {shippingRates.map((rate, index) => (
+            <li key={index}>
+              <strong>Transporteur:</strong> {rate.provider} <br />
+              <strong>Montant:</strong> {rate.amount} {rate.currency} <br />
+              <hr />
+            </li>
+          ))}
+        </ul>
+      )}
+      {error && <p className='text-red-600'>{error}</p>}
+    </div>
+  );
+};
+
+export default HomePage;

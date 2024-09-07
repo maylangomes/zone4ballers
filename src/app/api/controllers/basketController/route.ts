380bf5710efaf1024a2f1db85a591b6b46d54291
Maylan <maylan.gomes@epitech.eu>
2024-09-07 11:41:40 +0200

    fix: basketController return response si itemlist vide

--- src/app/api/controllers/basketController/route.ts
+++ src/app/api/controllers/basketController/route.ts
@@ -1,12 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
+  const { itemList } = await request.json();
+  if (itemList === null) {
+    return NextResponse.json({ message: 'itemList empty' }, { status: 200 });
+  }
   try {
-    const { itemList } = await request.json();
     const itemId = itemList.map((item: { id: any }) => item.id);
 
     return NextResponse.json({ message: 'itemList received' }, { status: 200 });
   } catch (error) {
     console.error('Error try fetching itemList', error);
+    return NextResponse.json(
+      { message: 'no response basket' },
+      { status: 500 },
+    );
   }
 }
